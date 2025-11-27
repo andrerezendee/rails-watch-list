ccb1464da6285360a268057add646d1fef3767b6
andre rezende <andre_fmr89@hotmail.com>
2025-11-27 13:13:55 -0300

    exer 2

---
+++ app/models/bookmark.rb
@@ -0,0 +1,15 @@
+# class Bookmark < ApplicationRecord
+#   belongs_to :movie
+#   belongs_to :list
+#   validates :comment, presence: true, lenght: { minimum: 6}
+#   validates :movie_id, uniqueness: {scope: :list_id, message: "is already in the list"}
+#   validates :list, presence: true
+
+# end
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
+  validates :list, presence: true
+end
